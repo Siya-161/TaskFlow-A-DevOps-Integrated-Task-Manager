dfad54fb526813218853fdbf131c83c850ea88be
Siya-161 <104910960+Siya-161@users.noreply.github.com>
2025-04-01 20:06:20 +0200

    Local files upload
    Uploaded local files from desktop to repository

---
+++ BACKEND/server.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+//connect to MongoDB
+mongoose
+    .connect(process.env.MONGO_URI, { useNewURLParser: true})
+    .then(() => console.log("MongoDB connected"))
+    .catch((err) => console.log(err));
+
+//sample route
+app.get("/", (req, res) => {
+    res.send("MERN Stack Backend is running");
+});
+
+//start server
+const Port = process.env || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

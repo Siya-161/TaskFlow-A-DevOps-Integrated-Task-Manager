c60305200f2b773b97f35ae152759b793e6bd435
Siya-161 <104910960+Siya-161@users.noreply.github.com>
2025-04-09 19:58:28 +0200

    File configuration to get server to run
    Configured my package.json and package-lock.json by making the necessary adjustments in order to get my server to run

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 //connect to MongoDB
 mongoose
-    .connect(process.env.MONGO_URI, { useNewURLParser: true})
+    .connect(process.env.MONGO_URI, { useNewUrlParser: true})
     .then(() => console.log("MongoDB connected"))
     .catch((err) => console.log(err));
 
@@ -21,5 +21,5 @@ app.get("/", (req, res) => {
 });
 
 //start server
-const Port = process.env || 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
